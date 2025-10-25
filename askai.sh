b1b7596dc326d7dc8ab38f85f363b9467e7a47ea
Josh Garber <josh@jkgarber.com>
2025-10-25 12:09:06 +0200

    abstract api url and api key

--- askai.sh
+++ askai.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 MODEL=""
+RESOURCE=""
+API_KEY=""
+OPENAI="https://api.openai.com/v1/responses"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -8,9 +11,13 @@ while [[ $# -gt 0 ]]; do
             case $2 in
                 "gpt-4.1-mini")
                     MODEL="$2"
+                    RESOURCE="$OPENAI"
+                    API_KEY="$OPENAI_API_KEY"
                 ;;
                 "gpt-4.1-nano")
                     MODEL="$2"
+                    RESOURCE=$OPENAI
+                    API_KEY="$OPENAI_API_KEY"
                 ;;
                 *)
                     echo "Unknown model: $2"
@@ -28,15 +35,17 @@ done
 
 if [[ -z "$MODEL" ]]; then
     MODEL="gpt-4.1-nano"
+    RESOURCE=$OPENAI
+    API_KEY="$OPENAI_API_KEY"
 fi
 
 echo "Hello, how can I help?"
 
 read -r QUESTION
 
-RESPONSE=$(curl --silent https://api.openai.com/v1/responses \
+RESPONSE=$(curl --silent $RESOURCE \
     -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $OPENAI_API_KEY" \
+    -H "Authorization: Bearer $API_KEY" \
     -d "{\"model\": \"$MODEL\", \"input\": \"$QUESTION\"}")
 
 echo "$RESPONSE" | jq -r '.output[0].content[0].text'
